bf1ce63a5e1a75b0177cf6e3ab7968b77c3a18f6
mchaou2s <mchaou2s@vm-2d21.inf.h-brs.de>
2023-11-12 18:25:45 +0100

    Abgabe 4.1 teilweise

---
+++ Uebung_4.1.js
@@ -0,0 +1,20 @@
+function identity(a){
+    return a;
+}
+
+
+function identity_function(a){
+    return identity(a);
+}
+
+function add(a,b){
+    return a+b;
+}
+
+function mul(a,b){
+    return a*b;
+}
+
+addf(x){
+    return 
+}
